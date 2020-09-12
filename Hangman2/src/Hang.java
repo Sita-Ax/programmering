b7c30d612a2449d7faa787fca16f7bf860a28af8
Sita-Ax <69907961+Sita-Ax@users.noreply.github.com>
2020-09-12 19:31:24 +0200

    Add files via upload

---
+++ Hangman2/src/Hang.java
@@ -0,0 +1,16 @@
+
+
+public class Hang {
+
+	private static String[] words = { "family", "bestfriends", "computer", "fruit", "teacher", "bag" };//an array
+	private static String word = words[(int) (Math.random() * words.length)];	//saved in random * 6
+
+	
+	public static String getWord() {									//this is the word
+		return word;
+	}
+	public static void setWord(String word) {
+		Hang.word = word;
+	}
+	
+}
